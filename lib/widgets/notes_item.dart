ec888f030ac07eeb89d011230217f6026ba365fa
hassanmoh262 <hassanmoh262@gmail.com>
2026-06-18 00:11:05 +0300

    create edit note view

--- lib/widgets/notes_item.dart
+++ lib/widgets/notes_item.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
   NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 10, right: 10),
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        borderRadius: BorderRadius.circular(16),
-      ),
-
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              "Flutter Tips",
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Text(
-              "build your hobba with ",
-              style: TextStyle(color: Colors.black.withValues(alpha: .5)),
-            ),
-            trailing: Icon(Icons.delete, color: Colors.black),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
           ),
-          Text("1 may 2026", style: TextStyle(color: Colors.black)),
-        ],
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 10, right: 10),
+        decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.circular(16),
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                "Flutter Tips",
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Text(
+                "build your hobba with ",
+                style: TextStyle(color: Colors.black.withValues(alpha: .5)),
+              ),
+              trailing: Icon(Icons.delete, color: Colors.black),
+            ),
+            Text("1 may 2026", style: TextStyle(color: Colors.black)),
+          ],
+        ),
       ),
     );
   }
